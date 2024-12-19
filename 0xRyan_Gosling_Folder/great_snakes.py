6978cb8229456118d5525eb30c98480f5ee9f50b
Abdulrhman <abdulrhmanamro7180@gmail.com>
2024-12-19 21:08:26 +0200

    Hello meet my encryption snake, look out! it is danger!

--- 0xRyan_Gosling_Folder/great_snakes.py
+++ 0xRyan_Gosling_Folder/great_snakes.py
@@ -1,5 +1,5 @@
 # Ascii
-
+#
 # ascii_array = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]  # Example ASCII values
 
 # # Convert ASCII values to characters
